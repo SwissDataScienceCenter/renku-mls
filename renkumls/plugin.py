a79fa1613545f592f644054e95b7af087fca8161
Viktor Gal <viktor.gal@maeth.com>
2021-04-22 16:23:10 +0200

    add proper error handling when provenance graph has not been generated

--- renkumls/plugin.py
+++ renkumls/plugin.py
@@ -28,6 +28,7 @@ from renku.core.models.cwl.annotation import Annotation
 from renku.core.incubation.command import Command
 from renku.core.plugins import hookimpl
 from renku.core.models.provenance.provenance_graph import ProvenanceGraph
+from renku.core.errors import RenkuException
 
 from prettytable import PrettyTable
 from deepdiff import DeepDiff
@@ -66,31 +67,34 @@ def process_run_annotations(run):
         )
         p.unlink()
         annotations.append(
-            Annotation(
-                id=annotation_id,
-                source="MLS plugin",
-                body=mls_annotation
-            )
+            Annotation(id=annotation_id, source="MLS plugin", body=mls_annotation)
         )
     return annotations
+
 
 def _run_id(activity_id):
     return str(activity_id).split("/")[-1]
 
 
+def _load_provenance_graph(client):
+    if not client.provenance_graph_path.exists():
+        raise RenkuException(
+            """Provenance graph has not been generated!
+Please run 'renku graph generate' to create the project's provenance graph
+"""
+        )
+    return ProvenanceGraph.from_json(client.provenance_graph_path)
+
+
 def _graph(revision, paths):
     # FIXME: use (revision, paths) filter
-    cmd_result = Command().command(
-        lambda client: ProvenanceGraph.from_json(client.provenance_graph_path)
-    ).build().execute()
-    if cmd_result.status == cmd_result.FAILURE:
-        raise Exception(f"Error creating ProvenanceGraph: {cmd_result.error}!")
+    cmd_result = Command().command(_load_provenance_graph).build().execute()
 
     provenance_graph = cmd_result.output
     provenance_graph.custom_bindings = {
         "mls": "http://www.w3.org/ns/mls#",
         "oa": "http://www.w3.org/ns/oa#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#"
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
     }
     return provenance_graph
 
@@ -98,13 +102,13 @@ def _graph(revision, paths):
 def _create_leaderboard(data, metric, format=None):
     leaderboard = PrettyTable()
     leaderboard.field_names = ["Run ID", "Model", "Inputs", metric]
-    leaderboard.align["Model"] = 'l'
-    leaderboard.align["Inputs"] = 'l'
-    leaderboard.align[metric] = 'r'
+    leaderboard.align["Model"] = "l"
+    leaderboard.align["Inputs"] = "l"
+    leaderboard.align[metric] = "r"
     for commit, v in data.items():
         if metric in v:
-            v['inputs'].sort()
-            leaderboard.add_row([commit, v["model"], v['inputs'], v[metric]])
+            v["inputs"].sort()
+            leaderboard.add_row([commit, v["model"], v["inputs"], v[metric]])
     leaderboard.sortby = metric
     leaderboard.reversesort = True
     return leaderboard
@@ -116,7 +120,11 @@ def mls():
 
 
 @mls.command()
-@click.option("--revision", default="HEAD", help="The git revision to generate the log for, default: HEAD")
+@click.option(
+    "--revision",
+    default="HEAD",
+    help="The git revision to generate the log for, default: HEAD",
+)
 @click.option("--format", default="ascii", help="Choose an output format.")
 @click.option("--metric", default="accuracy", help="Choose metric for the leaderboard")
 @click.argument("paths", type=click.Path(exists=False), nargs=-1)
@@ -124,42 +132,51 @@ def leaderboard(revision, format, metric, paths):
     """Leaderboard based on evaluation metrics of machine learning models"""
     graph = _graph(revision, paths)
     leaderboard = dict()
-    for r in graph.query("""SELECT DISTINCT ?type ?value ?run ?runId ?dsPath where {{
+    for r in graph.query(
+        """SELECT DISTINCT ?type ?value ?run ?runId ?dsPath where {{
         ?em a mls:ModelEvaluation ;
         mls:hasValue ?value ;
         mls:specifiedBy ?type ;
         ^mls:hasOutput/mls:implements/rdfs:label ?run ;
         ^mls:hasOutput/^oa:hasBody/oa:hasTarget ?runId ;
         ^mls:hasOutput/^oa:hasBody/oa:hasTarget/prov:qualifiedUsage/prov:entity/prov:atLocation ?dsPath
-        }}"""):
+        }}"""
+    ):
         run_id = _run_id(r.runId)
         metric_type = r.type.split("#")[1]
         if run_id in leaderboard:
-            leaderboard[run_id]['inputs'].append(r.dsPath.__str__())
+            leaderboard[run_id]["inputs"].append(r.dsPath.__str__())
             continue
         leaderboard[run_id] = {
-                metric_type: r.value.value,
-                "model": r.run,
-                "inputs": [r.dsPath.__str__()]
-            }
+            metric_type: r.value.value,
+            "model": r.run,
+            "inputs": [r.dsPath.__str__()],
+        }
     if len(paths):
         filtered_board = dict()
         for path in paths:
-            filtered_board.update(dict(filter(
-                lambda x: path in x[1]["inputs"], leaderboard.items()
-            )))
+            filtered_board.update(
+                dict(filter(lambda x: path in x[1]["inputs"], leaderboard.items()))
+            )
         print(_create_leaderboard(filtered_board, metric))
     else:
         print(_create_leaderboard(leaderboard, metric))
 
 
 @mls.command()
-@click.option("--revision", default="HEAD", help="The git revision to generate the log for, default: HEAD")
+@click.option(
+    "--revision",
+    default="HEAD",
+    help="The git revision to generate the log for, default: HEAD",
+)
 @click.option("--format", default="ascii", help="Choose an output format.")
-@click.option("--diff", nargs=2, help="Print the difference between two model revisions")
+@click.option(
+    "--diff", nargs=2, help="Print the difference between two model revisions"
+)
 @click.argument("paths", type=click.Path(exists=False), nargs=-1)
 def params(revision, format, paths, diff):
     """List the hyper-parameter settings of machine learning models"""
+
     def _param_value(rdf_iteral):
         if not type(rdf_iteral) != rdflib.term.Literal:
             return rdf_iteral
@@ -168,10 +185,10 @@ def params(revision, format, paths, diff):
         else:
             return rdf_iteral.toPython()
 
-
     graph = _graph(revision, paths)
     model_params = dict()
-    for r in graph.query("""SELECT ?runId ?algo ?hp ?value where {{
+    for r in graph.query(
+        """SELECT ?runId ?algo ?hp ?value where {{
         ?run a mls:Run ;
         mls:hasInput ?in .
         ?in a mls:HyperParameterSetting .
@@ -179,15 +196,15 @@ def params(revision, format, paths, diff):
         ?in mls:hasValue ?value .
         ?run mls:implements/rdfs:label ?algo ;
         ^oa:hasBody/oa:hasTarget ?runId
-        }}"""):
+        }}"""
+    ):
         run_id = _run_id(r.runId)
         if run_id in model_params:
             model_params[run_id]["hp"][str(r.hp)] = _param_value(r.value)
         else:
-            model_params[run_id] = dict({
-                "algorithm": str(r.algo),
-                "hp": {str(r.hp): _param_value(r.value)}
-            })
+            model_params[run_id] = dict(
+                {"algorithm": str(r.algo), "hp": {str(r.hp): _param_value(r.value)}}
+            )
 
     if len(diff) > 0:
         for r in diff:
@@ -199,27 +216,31 @@ def params(revision, format, paths, diff):
             print("\t- {}".format(model_params[diff[0]]["algorithm"]))
             print("\t+ {}".format(model_params[diff[1]]["algorithm"]))
         else:
-            params_diff = DeepDiff(model_params[diff[0]], model_params[diff[1]], ignore_order=True)
+            params_diff = DeepDiff(
+                model_params[diff[0]], model_params[diff[1]], ignore_order=True
+            )
             output = PrettyTable()
             output.field_names = ["Hyper-Parameter", "Old", "New"]
-            output.align["Hyper-Parameter"] = 'l'
+            output.align["Hyper-Parameter"] = "l"
             if "values_changed" not in params_diff:
                 print(output)
                 return
             for k, v in params_diff["values_changed"].items():
                 parameter_name = re.search(r"\['(\w+)'\]$", k).group(1)
-                output.add_row([
-                    parameter_name,
-                    _param_value(v['new_value']),
-                    _param_value(v['old_value'])
-                ])
+                output.add_row(
+                    [
+                        parameter_name,
+                        _param_value(v["new_value"]),
+                        _param_value(v["old_value"]),
+                    ]
+                )
             print(output)
     else:
         output = PrettyTable()
         output.field_names = ["Run ID", "Model", "Hyper-Parameters"]
-        output.align["Run ID"] = 'l'
-        output.align["Model"] = 'l'
-        output.align["Hyper-Parameters"] = 'l'
+        output.align["Run ID"] = "l"
+        output.align["Model"] = "l"
+        output.align["Hyper-Parameters"] = "l"
         for runid, v in model_params.items():
             output.add_row([runid, v["algorithm"], json.dumps(v["hp"])])
         print(output)
